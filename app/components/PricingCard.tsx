dde45dac6de6fe1ee5a83980c35bcb7d137ddfa4
Ian Mathaiya <iannmMathaiya@gmail.com>
2023-05-09 16:40:50 +0300

    selected radio button animation

--- app/components/PricingCard.tsx
+++ app/components/PricingCard.tsx
@@ -1,8 +1,8 @@
 "use client"
-import { Check } from 'lucide-react'
-import { CircleFill, RecordCircleFill } from 'react-bootstrap-icons'
 import { useState } from "react"
+import { Check } from 'lucide-react'
 import { RadioGroup } from '@headlessui/react'
+import { CircleFill, RecordCircleFill } from 'react-bootstrap-icons'
 
 const plans = [
     {
@@ -31,13 +31,12 @@ const plans = [
     }
 ]
 
-
 export default function PricingCard() {
 
-    const [selected, setSelected] = useState(plans[0])
+    const [selected, setSelected] = useState()
 
     return (
-        <div className="py-4 px-4 lg:px-8 bg-white w-96 md:w-2/4 lg:w-2/6 shadow-xl rounded-xl">
+        <div className="m-4 py-4 px-4 lg:px-8 bg-white w-96 md:w-4/6 lg:w-2/6 h-fit drop-shadow-xl rounded-xl">
             <div className="flex items-center justify-start gap-x-4">
                 <h1 className="text-xl font-bold text-slate-800">Subscription</h1>
                 <h4 className="text-xs bg-pink-600 px-2 py-1 text-white rounded-md">✨ 50% Off</h4>
@@ -50,80 +49,75 @@ export default function PricingCard() {
 
             <div className="mt-4 w-full border-t-2 border-slate-200/40" aria-hidden="true" />
 
-            <div className="w-full px-2 py-4">
+            <div className="relative w-full px-2 py-4">
 
-                <RadioGroup value={selected} onChange={setSelected}>
-                    <RadioGroup.Label className='sr-only'>Plans</RadioGroup.Label>
-                    <div className='flex flex-col gap-y-4'>
-                        {plans.map(plan => (
-                            <RadioGroup.Option
-                                key={plan.id}
-                                value={plan}
-                                className={({ active, checked }) =>
-                                    `${active ? 'border-2 border-pink-600' : 'bg-white'} 
-                                ${checked ? 'shadow' : 'shadow-none'} border-2 rounded-lg p-2`
-                                }>
-                                {({ active, checked }) => (
-                                    <div className='flex items-start gap-x-2'>
+                <form>
+                    <RadioGroup value={selected} onChange={setSelected}>
+                        <RadioGroup.Label className='sr-only'>Plans</RadioGroup.Label>
+                        <div className='flex flex-col gap-y-4'>
+                            {plans.map(plan => (
+                                <RadioGroup.Option
+                                    key={plan.id}
+                                    value={plan}
+                                    className={({ active, checked }) =>
+                                        `${active ? 'border-2 border-pink-400' : 'bg-white'} 
+                                        ${checked ? 'absolute z-10 top-10 inset-x-0 ease-in translate-y-10 scale-105 duration-300 w-full drop-shadow' : 'drop-shadow-none'} bg-white border-2 rounded-lg p-2`
+                                    }>
+                                    {({ active, checked }) => (
+                                        <div className='flex items-start gap-x-2'>
 
-                                   
-                                        {checked && (
-                                            <div className="shrink-0 text-pink-600">
-                                                <RecordCircleFill className="h-6 w-6" aria-hidden='true' />
-                                            </div>
-                                        )}
+                                            {checked && (
+                                                <div className="shrink-0 text-pink-600">
+                                                    <RecordCircleFill className="h-6 w-6" aria-hidden='true' />
+                                                </div>
+                                            )}
 
-                                        {!checked && (
-                                            <div className="shrink-0 text-slate-100">
-                                                <CircleFill className="h-6 w-6" aria-hidden='true' />
-                                            </div>
-                                        )}
-                                    
+                                            {!checked && (
+                                                <div className="shrink-0 text-slate-100">
+                                                    <CircleFill className="h-6 w-6" aria-hidden='true' />
+                                                </div>
+                                            )}
 
-                                        <div className='w-full'>
-                                            <div className="flex items-center justify-between text-sm text-slate-900">
-                                                <RadioGroup.Label as='h1'className='font-semibold'> {plan.name}
-                                                </RadioGroup.Label>
 
-                                                <RadioGroup.Description as='h1'  className='font-medium'>
-                                                    $ {plan.price}
+                                            <div className='w-full'>
+                                                <div className="flex items-center justify-between text-sm text-slate-900">
+                                                    <RadioGroup.Label as='h1' className='font-semibold'> {plan.name}
+                                                    </RadioGroup.Label>
+
+                                                    <RadioGroup.Description as='h1' className='font-medium'>
+                                                        $ {plan.price}
+                                                    </RadioGroup.Description>
+                                                </div>
+
+                                                <RadioGroup.Description as='p' className="text-xs md:text-sm text-slate-500 font-normal">
+                                                    {plan.description}
                                                 </RadioGroup.Description>
-                                            </div>
 
-                                            <RadioGroup.Description as='p' className="text-xs md:text-sm text-slate-500 font-normal">
-                                                {plan.description}
-                                            </RadioGroup.Description>
+                                                <div className='my-3'>
+                                                    <div className="flex items-center gap-x-2">
+                                                        <Check className='w-4 h-4 text-pink-600' />
+                                                        <p className='text-sm text-slate-700'>{plan.perks.space}TB of space</p>
+                                                    </div>
 
-                                            <div className='my-3'>
-                                                <div className="flex items-center gap-x-2">
-                                                    <Check className='w-4 h-4 text-pink-600' />
-                                                    <p className='text-sm text-slate-700'>{plan.perks.space}TB of space</p>
-                                                </div>
+                                                    <div className="flex items-center gap-x-2">
+                                                        <Check className='w-4 h-4 text-pink-600' />
+                                                        <p className='text-sm text-slate-700'>{plan.perks.fileRecovery} days of file recovery</p>
+                                                    </div>
 
-                                                <div className="flex items-center gap-x-2">
-                                                    <Check className='w-4 h-4 text-pink-600' />
-                                                    <p className='text-sm text-slate-700'>{plan.perks.fileRecovery} days of file recovery</p>
-                                                </div>
-
-                                                <div className="flex items-center gap-x-2">
-                                                    <Check className='w-4 h-4 text-pink-600' />
-                                                    <p className='text-sm text-slate-700'>{plan.perks.unlimitedHistory ? 'Unlimited undo history' : 'Limited undo history'}</p>
+                                                    <div className="flex items-center gap-x-2">
+                                                        <Check className='w-4 h-4 text-pink-600' />
+                                                        <p className='text-sm text-slate-700'>{plan.perks.unlimitedHistory ? 'Unlimited undo history' : 'Limited undo history'}</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-
-                                    </div>
-                                )}
-
-                            </RadioGroup.Option>
-                        ))}
-
-                    </div>
-
-                </RadioGroup>
-
+                                    )}
+                                </RadioGroup.Option>
+                            ))}
+                        </div>
+                    </RadioGroup>
+                </form>
             </div>
-
-        </div>
+        </div >
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import PricingCard from './components/PricingCard'
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center bg-slate-200">
+      <div className="m-0 max-w-full w-full h-screen flex items-center justify-center bg-slate-200">
         <PricingCard />
 
       </div>
